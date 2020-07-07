20c9bbc2be1ae11416e04d68e4a8a4f460e2b8eb
flowzGenius <217010298@stud.cut.ac.za>
2020-07-07 20:10:31 +0200

    Entities project added to model the database. Code first approach ef core.

---
+++ CommandLineStore.Entities/CommandLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandLineStore.Entities
+{
+    public class CommandLine
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string HowTo { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Line { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string PlatForm { get; set; }
+    }
+}
