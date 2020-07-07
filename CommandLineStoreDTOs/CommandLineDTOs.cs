9723ae5a9160435b05c821d214e59fb16d5f0a7f
flowzGenius <217010298@stud.cut.ac.za>
2020-07-07 22:36:09 +0200

    Added DTO library

---
+++ CommandLineStoreDTOs/CommandLineDTOs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandLineStoreDTOs
+{
+    public class CommandLineCreateDto
+    {
+        [Required(ErrorMessage ="This field is required")]
+        public string HowTo { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Line { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string PlatForm { get; set; }
+    }
+
+    public class CommandLineOperationDto
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string HowTo { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Line { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string PlatForm { get; set; }
+    }
+
+    public class CommandLineViewDto
+    {
+        public string Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string PlatForm { get; set; }
+    }
+}
