707e34d555f05c123da513187f8d11fb0226ee3d
flowzGenius <217010298@stud.cut.ac.za>
2020-07-07 22:35:38 +0200

    Added initial db migration

---
+++ CommandLineStore.DataContext/Migrations/20200707202526_initialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommandLineStore.DataContext.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommandLines",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    HowTo = table.Column<string>(maxLength: 500, nullable: false),
+                    Line = table.Column<string>(maxLength: 100, nullable: false),
+                    PlatForm = table.Column<string>(maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommandLines", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommandLines");
+        }
+    }
+}
