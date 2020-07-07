431fdd9b3f28a7798b444ee99b028558c3841cd7
flowzGenius <217010298@stud.cut.ac.za>
2020-07-07 20:22:30 +0200

    Added ef Contxet of the database

--- CommandLineStore.DataContext/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CommandLineStore.DataContext
-{
-    public class Class1
-    {
-    }
-}
---
+++ CommandLineStore.DataContext/CommandContext.cs
@@ -0,0 +1,16 @@
+﻿using CommandLineStore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CommandLineStore.DataContext
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<CommandLine> CommandLines { get; set; }
+    }
+}
